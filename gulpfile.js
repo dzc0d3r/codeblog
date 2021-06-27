5c05acb341a6813e20bfc2a1213cce17fc81c03a
dzc0d3r <m4dcod3r@protonmail.com>
2021-06-27 18:49:59 +0100

    delete bootstrap files; add gulp and gulp-imagemin; minified images

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require("gulp")
+const imagemin = require("gulp-imagemin")
+
+
+function imgSquash() {
+   return gulp
+      .src("./assets/images/*")
+      .pipe(imagemin())
+      .pipe(gulp.dest("./assets/images/minified"))
+}
+
+gulp.task("imgSquash", imgSquash)
+
+gulp.task("watch", () => {
+   gulp.watch("./assets/images/*", imgSquash)
+})
+
+gulp.task("default", gulp.series("imgSquash"))
